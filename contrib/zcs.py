3e588fe27ecac9146e7b2d672449c65623281d11
Chris O'Hara <cohara87@gmail.com>
2017-02-17 17:51:09 +1000

    Only specify ctypes return type when necessary

--- contrib/zcs.py
+++ contrib/zcs.py
@@ -43,45 +43,38 @@ Example read (C):
     }
 """
 
-import ctypes
+from ctypes import cdll
+from ctypes import c_char_p, c_size_t, c_void_p, c_int, c_int32, c_int64, c_bool
 import platform
 
 shared_lib_ext = "dylib" if platform.uname()[0] == "Darwin" else "so"
 
-libzcs = ctypes.cdll.LoadLibrary("libzcs.%s" % shared_lib_ext)
+libzcs = cdll.LoadLibrary("libzcs.%s" % shared_lib_ext)
 
 zcs_writer_new = libzcs.zcs_writer_new
-zcs_writer_new.argtypes = [ctypes.c_char_p, ctypes.c_size_t]
-zcs_writer_new.restype = ctypes.c_void_p
+zcs_writer_new.argtypes = [c_char_p, c_size_t]
+zcs_writer_new.restype = c_void_p
 
 zcs_writer_free = libzcs.zcs_writer_free
-zcs_writer_free.argtypes = [ctypes.c_void_p]
+zcs_writer_free.argtypes = [c_void_p]
 
 zcs_writer_add_column = libzcs.zcs_writer_add_column
-zcs_writer_add_column.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_int,
-                                  ctypes.c_int, ctypes.c_int]
-zcs_writer_add_column.restype = ctypes.c_bool
+zcs_writer_add_column.argtypes = [c_void_p, c_int, c_int, c_int, c_int]
 
 zcs_writer_put_bit = libzcs.zcs_writer_put_bit
-zcs_writer_put_bit.argtypes = [ctypes.c_void_p, ctypes.c_bool]
-zcs_writer_put_bit.restype = ctypes.c_bool
+zcs_writer_put_bit.argtypes = [c_void_p, c_bool]
 
 zcs_writer_put_i32 = libzcs.zcs_writer_put_i32
-zcs_writer_put_i32.argtypes = [ctypes.c_void_p, ctypes.c_size_t, ctypes.c_int32]
-zcs_writer_put_i32.restype = ctypes.c_bool
+zcs_writer_put_i32.argtypes = [c_void_p, c_size_t, c_int32]
 
 zcs_writer_put_i64 = libzcs.zcs_writer_put_i64
-zcs_writer_put_i64.argtypes = [ctypes.c_void_p, ctypes.c_size_t, ctypes.c_int64]
-zcs_writer_put_i64.restype = ctypes.c_bool
+zcs_writer_put_i64.argtypes = [c_void_p, c_size_t, c_int64]
 
 zcs_writer_put_str = libzcs.zcs_writer_put_str
-zcs_writer_put_str.argtypes = [ctypes.c_void_p, ctypes.c_size_t,
-                               ctypes.c_char_p]
-zcs_writer_put_str.restype = ctypes.c_bool
+zcs_writer_put_str.argtypes = [c_void_p, c_size_t, c_char_p]
 
 zcs_writer_finish = libzcs.zcs_writer_finish
-zcs_writer_finish.argtypes = [ctypes.c_void_p, ctypes.c_bool]
-zcs_writer_finish.restype = ctypes.c_bool
+zcs_writer_finish.argtypes = [c_void_p, c_bool]
 
 BIT = 0
 I32 = 1
@@ -108,17 +101,15 @@ class Writer(object):
         self.row_group_size = row_group_size
         self.sync = sync
         self.column_types = [column.type for column in columns]
-        self.writer = None
-
         self.put_by_type = [self.put_bit, self.put_i32, self.put_i64,
                             self.put_str]
+        self.writer = None
 
     def __enter__(self):
         assert self.writer is None
         self.writer = zcs_writer_new(self.path, self.row_group_size)
         if not self.writer:
             raise RuntimeError("failed to create writer for %s" % self.path)
-
         for column in self.columns:
             if not zcs_writer_add_column(self.writer, column.type,
                                          column.encoding, column.compression,
