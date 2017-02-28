c43d551b473e7369b51b19a217fde4174faf90f9
Chris O'Hara <cohara87@gmail.com>
2017-02-28 19:44:49 +1300

    DRY up code with helpers

--- include/java.h
+++ include/java.h
@@ -9,4 +9,11 @@ void Java_zcs_jni_Reader_nativeFree(JNIEnv *, jobject, jlong);
 
 jlong Java_zcs_jni_Reader_nativeColumnCount(JNIEnv *, jobject, jlong);
 
+jint Java_zcs_jni_Reader_nativeColumnType(JNIEnv *, jobject, jlong, jint);
+
+jint Java_zcs_jni_Reader_nativeColumnEncoding(JNIEnv *, jobject, jlong, jint);
+
+jint Java_zcs_jni_Reader_nativeColumnCompression(JNIEnv *, jobject, jlong,
+                                                 jint);
+
 #endif
--- lib/java.c
+++ lib/java.c
@@ -32,30 +32,76 @@ jlong Java_zcs_jni_Reader_nativeNew(JNIEnv *env, jobject this,
         goto error;
     return (jlong)reader;
 error:
-    zcs_java_throw(env, "java/lang/IllegalArgumentException",
-                   "zcs_reader_new()");
+    zcs_java_throw(env, "java/lang/Exception", "zcs_reader_new()");
     return 0;
+}
+
+static struct zcs_reader *zcs_java_reader_cast(JNIEnv *env, jlong ptr)
+{
+    struct zcs_reader *reader = (struct zcs_reader *)ptr;
+    if (!reader)
+        zcs_java_npe(env, "reader ptr");
+    return reader;
 }
 
 void Java_zcs_jni_Reader_nativeFree(JNIEnv *env, jobject this, jlong ptr)
 {
-    struct zcs_reader *reader = (struct zcs_reader *)ptr;
+    struct zcs_reader *reader = zcs_java_reader_cast(env, ptr);
     if (!reader)
-        goto error;
+        return;
     zcs_reader_free(reader);
-    return;
-error:
-    zcs_java_npe(env, "zcs_reader_free()");
 }
 
 jlong Java_zcs_jni_Reader_nativeColumnCount(JNIEnv *env, jobject this,
                                             jlong ptr)
 {
-    struct zcs_reader *reader = (struct zcs_reader *)ptr;
+    struct zcs_reader *reader = zcs_java_reader_cast(env, ptr);
     if (!reader)
-        goto error;
+        return 0;
     return zcs_reader_column_count(reader);
-error:
-    zcs_java_npe(env, "zcs_reader_column_count()");
-    return 0;
+}
+
+static bool zcs_java_reader_check_column_bounds(JNIEnv *env,
+                                                const struct zcs_reader *reader,
+                                                jint index)
+{
+    if (index < 0 || index >= zcs_reader_column_count(reader)) {
+        zcs_java_throw(env, "java/lang/IndexOutOfBoundsException",
+                       "column index out of bounds");
+        return false;
+    }
+    return true;
+}
+
+jint Java_zcs_jni_Reader_nativeColumnType(JNIEnv *env, jobject this, jlong ptr,
+                                          jint index)
+{
+    struct zcs_reader *reader = zcs_java_reader_cast(env, ptr);
+    if (!reader)
+        return 0;
+    if (!zcs_java_reader_check_column_bounds(env, reader, index))
+        return 0;
+    return zcs_reader_column_type(reader, index);
+}
+
+jint Java_zcs_jni_Reader_nativeColumnEncoding(JNIEnv *env, jobject this,
+                                              jlong ptr, jint index)
+{
+    struct zcs_reader *reader = zcs_java_reader_cast(env, ptr);
+    if (!reader)
+        return 0;
+    if (!zcs_java_reader_check_column_bounds(env, reader, index))
+        return 0;
+    return zcs_reader_column_encoding(reader, index);
+}
+
+jint Java_zcs_jni_Reader_nativeColumnCompression(JNIEnv *env, jobject this,
+                                                 jlong ptr, jint index)
+{
+    struct zcs_reader *reader = zcs_java_reader_cast(env, ptr);
+    if (!reader)
+        return 0;
+    if (!zcs_java_reader_check_column_bounds(env, reader, index))
+        return 0;
+    return zcs_reader_column_compression(reader, index);
 }
